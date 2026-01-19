f4da6d051b44a8b459759b648646355725d6523d
gusdnrs <gusdnrs@naver.com>
2026-01-19 11:46:30 +0900

    Style update : 2026-01-19

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
-export default function Square({value, onSquareClick}) { // props로 value와 onSquareClick 받음
-  return <button 
-          type="button" 
-          className="square" 
-          onClick={onSquareClick} // 클릭 시 onSquareClick 함수 실행
+export default function Square({ value, onSquareClick }) { // props로 value와 onSquareClick 받음
+  return <button
+    type="button"
+    className={`square ${value === 'X' ? 'is-x' : value === 'O' ? 'is-o' : ''}`}
+    onClick={onSquareClick} // 클릭 시 onSquareClick 함수 실행
   >
     {value}
   </button>
